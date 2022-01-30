5b17b223c6898edda9b78dbd7ccf10f03d8541d9
Scott W Harden <swharden@gmail.com>
2022-01-29 23:46:17 -0500

    UpdateGeneralMemberCount: regenerate JSON after update

--- src/CbSlackStats.Functions/UpdateGeneralMemberCount.cs
+++ src/CbSlackStats.Functions/UpdateGeneralMemberCount.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using CbSlackStats.Functions.TableEntities;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Table;
 
 namespace CbSlackStats.Functions
@@ -27,8 +29,23 @@ namespace CbSlackStats.Functions
             TableEntities.GeneralMemberCount entity = new(memberCount);
             TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(entity);
             await statsTable.ExecuteAsync(insertOrMergeOperation);
-
             log.LogInformation($"Successfully added a record to the table.");
+
+            var countsByDate = new System.Collections.Generic.SortedDictionary<DateTime, int>();
+            TableQuery<GeneralMemberCount> query = new();
+            foreach (GeneralMemberCount memberRecord in await statsTable.ExecuteQuerySegmentedAsync(query, null))
+            {
+                countsByDate.Add(memberRecord.DateTime, memberRecord.Count);
+            }
+            log.LogInformation($"Read and sorted {countsByDate.Count} records from the table.");
+
+            string json = Json.DatedCountsToJson(countsByDate);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("$web");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("general-member-count.json");
+            blockBlob.Properties.ContentType = "application/json";
+            await blockBlob.UploadTextAsync(json);
+            log.LogInformation($"Wrote web: general-member-count.json");
         }
     }
 }
