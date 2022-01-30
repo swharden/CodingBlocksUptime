c7d93d0e7dc71bfe6969f10aec590b1e618df721
Scott W Harden <swharden@gmail.com>
2022-01-30 13:58:41 -0500

    Stats: add support for generating image plots

---
+++ src/CbSlackStats.Tests/PlotTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CbSlackStats.Tests
+{
+    internal class PlotTests
+    {
+        [Test]
+        public void Test_Plot_Count()
+        {
+            string jsonFilePath = Path.Combine(SampleData.DataFolder, "general-member-count.json");
+            string json = File.ReadAllText(jsonFilePath);
+            SortedDictionary<DateTime, int> counts = Json.DatedCountsFromJson(json);
+
+            byte[] bytes = Plot.GeneratePng(600, 400, counts);
+            Assert.IsNotNull(bytes);
+            Assert.IsNotEmpty(bytes);
+
+            string filePath = Path.GetFullPath("test.png");
+            File.WriteAllBytes(filePath, bytes);
+            Console.WriteLine(filePath);
+        }
+    }
+}
---
+++ src/CbSlackStats/Plot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CbSlackStats
+{
+    public static class Plot
+    {
+        public static byte[] GeneratePng(int width, int height, SortedDictionary<DateTime, int> counts)
+        {
+            double[] xs = counts.Keys.Select(x => x.ToOADate()).ToArray();
+            double[] ys = counts.Values.Select(x => (double)x).ToArray();
+
+            var plt = new ScottPlot.Plot(width, height);
+            plt.Title("CodingBlocks Slack");
+            plt.YLabel("Members");
+            plt.AddScatterStep(xs, ys);
+            plt.XAxis.DateTimeFormat(true);
+
+            byte[] bytes = plt.GetImageBytes();
+            return bytes;
+        }
+    }
+}
