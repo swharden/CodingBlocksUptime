5f1c3e741453df4e483dd456f726ba0a2958a003
Scott W Harden <swharden@gmail.com>
2022-01-30 13:57:52 -0500

    Tests: create SampleData module

---
+++ src/CbSlackStats.Tests/SampleData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CbSlackStats.Tests
+{
+    internal static class SampleData
+    {
+        internal readonly static string DataFolder = GetDataFolder();
+
+        internal static string GetDataFolder(int maxLevelsUp = 10)
+        {
+            string dirname = Path.GetFullPath("./");
+            for (int i = 0; i < maxLevelsUp; i++)
+            {
+                string dataFolderPath = Path.Combine(dirname ?? "", "dev/sample-data");
+                if (Directory.Exists(dataFolderPath))
+                    return dataFolderPath;
+                else
+                    dirname = Path.GetDirectoryName(dirname) ?? "";
+            }
+
+            throw new InvalidOperationException("could not locate data folder");
+        }
+    }
+}
