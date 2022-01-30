9b26ac7add3bda77ff7bbe003ba5f990389d4ac5
Scott W Harden <swharden@gmail.com>
2022-01-29 21:34:48 -0500

    Test: JSON parsing of channel info

---
+++ src/CbSlackStats.Tests/JsonTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CbSlackStats.Tests
+{
+    internal class JsonTests
+    {
+        private readonly static string DataFolder = GetDataFolder();
+
+        private static string GetDataFolder(int maxLevelsUp = 10)
+        {
+            string dirname = Path.GetFullPath("./");
+            for (int i = 0; i < maxLevelsUp; i++)
+            {
+                string dataFolderPath = Path.Combine(dirname ?? "", "dev/sample-data");
+                if (Directory.Exists(dataFolderPath))
+                    return dataFolderPath;
+                else
+                    dirname = Path.GetDirectoryName(dirname) ?? "";
+            }
+
+            throw new InvalidOperationException("could not locate data folder");
+        }
+
+        [Test]
+        public void Test_JsonParsing_ChannelMemberCount()
+        {
+            string jsonFilePath = Path.Combine(DataFolder, "conversations.info.json");
+            Assert.That(File.Exists(jsonFilePath));
+            string txt = File.ReadAllText(jsonFilePath);
+            using JsonDocument doc = JsonDocument.Parse(txt);
+
+            Assert.IsTrue(SlackAPI.RequestIsOK(doc));
+            Assert.DoesNotThrow(() => SlackAPI.ThrowIfRequestIsNotOK(doc));
+            Assert.AreEqual(6401, SlackAPI.MemberCountFromChannelInfo(doc));
+        }
+    }
+}
