5bec1c647db550d9d085e8ab5ebcf942f3dfd379
Scott W Harden <swharden@gmail.com>
2022-01-29 19:11:07 -0500

    Test: authentication system

--- src/CbSlackStats.Tests/SlackApiTests.cs
+++ src/CbSlackStats.Tests/SlackApiTests.cs
@@ -5,4 +5,36 @@ using System.Threading.Tasks;
 
 namespace CbSlackStats.Tests
 {
+    public class SlackApiTests
+    {
+        const string TOKEN_ENV_VARNAME = "cbSlackStatsToken";
+        const string CB_GENERAL_CHANNEL_ID = "C0GL77VA6"; // found from conversations.list
+
+        [SetUp]
+        public void Setup_SetEnvironmentVariableFromUserSecret()
+        {
+            string token = new ConfigurationBuilder().AddUserSecrets<SlackApiTests>().Build()["slacktoken"];
+            if (token is not null)
+                Environment.SetEnvironmentVariable(TOKEN_ENV_VARNAME, token);
+        }
+
+        public static string GetTokenFromEnvironmentVariable()
+        {
+            return Environment.GetEnvironmentVariable(TOKEN_ENV_VARNAME) ??
+                throw new InvalidOperationException($"environment variables do not contain {TOKEN_ENV_VARNAME}");
+        }
+
+        [Test]
+        public void Test_Token_CanBeRetrieved()
+        {
+            string token = GetTokenFromEnvironmentVariable();
+            Assert.IsNotNull(token);
+        }
+
+        [Test]
+        public void Test_Token_IsProperFormat()
+        {
+            string token = GetTokenFromEnvironmentVariable();
+            Assert.That(token.StartsWith("xoxb-"));
+        }
 }
