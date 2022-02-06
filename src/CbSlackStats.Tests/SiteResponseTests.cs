6d0c4d1d598822947d3472f7129579bcc8f9c9bd
Scott W Harden <swharden@gmail.com>
2022-02-05 19:21:11 -0500

    Stats: GetResponseTime(url)

---
+++ src/CbSlackStats.Tests/SiteResponseTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CbSlackStats.Tests
+{
+    internal class SiteResponseTests
+    {
+        [Test]
+        public void Test_Website_Response()
+        {
+            string url = "http://example.com";
+            (int code, int length, double msec) = Website.GetResponseTime(url);
+            Console.WriteLine($"Request to {url} returned {code} with {length:N0} bytes in {msec:N3} ms");
+        }
+    }
+}
